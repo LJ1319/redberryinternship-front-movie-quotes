3952a41b42b48f280aa4e32491f3199730c5862c
LJ1319 <luka.jikia.2@btu.edu.ge>
2024-06-14 17:30:22 +0400

    fix: remove unnecessary sync method

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@ import { useI18n } from 'vue-i18n'
 import { setLocale } from '@vee-validate/i18n'
 
 import { useUserStore } from '@/stores/UserStore'
-import { useMovieStore } from '@/stores/MovieStore'
 import { getCookie, setCookie } from '@/utils/helpers'
 
 const router = useRouter()
@@ -13,7 +12,6 @@ const route = useRoute()
 const { locale } = useI18n()
 
 const userStore = useUserStore()
-const movieStore = useMovieStore()
 const localeCookie: string = getCookie('locale')
 
 const initLocale = () => {
@@ -32,20 +30,8 @@ if (localeCookie) {
   initLocale()
 }
 
-const syncData = () => {
-  switch (route.name) {
-    case 'my-movies':
-      movieStore.loadMovies()
-      break
-    case 'single-movie':
-      movieStore.loadMovie(route.params.id.toString())
-      break
-  }
-}
-
 watch(locale, () => {
   initLocale()
-  syncData()
 })
 
 userStore.$subscribe((mutation, state) => {
